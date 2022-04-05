ded805c0264b023d0a3e1a99f85495c3d6753558
Ankit <aarora1982@gmail.com>
2022-04-05 12:35:57 +0530

    ok

--- TravelGigAdenike/src/main/java/com/synex/controller/GatewayController.java
+++ TravelGigAdenike/src/main/java/com/synex/controller/GatewayController.java
@@ -76,6 +76,7 @@ public class GatewayController {
 	
 	@PostMapping(value="/saveInsurance2")
 	public ResponseEntity<String> saveInsurance1(@RequestBody InsuranceModel insuranceModel,Authentication authentication) {
+		//deleted line..
 		String response = insuranceService.saveInsurance(insuraceModel);
 		return new ResponseEntity<JsonNode>(responseNode,HttpStatus.OK);
 	}
